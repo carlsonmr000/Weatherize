91c3c94379069877f74c65fdbf4b650beb99fb41
Maxi Carlson <carlsonmr000@gmail.com>
2021-06-08 17:10:33 -0500

    made changes to readme

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const DOMAIN = 'https://openweathermap.org/api';
 const API_KEY = "09f877c9272bc64e109cd676a8422c51";
-const BASE_URL = `api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}`;
+const url = `aapi.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
+
+//variables
+
+const city = search.value
+
+try {
+
+    const result = await axios.get(url + city);
+    console.log(result);
+
+
+} catch (error) {
+    console.log(error.message);
+}
+
