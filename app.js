fd2aee0006a6f507b7d4f8cc067d66a01dfe8ff7
Maxi Carlson <carlsonmr000@gmail.com>
2021-06-08 15:26:25 -0500

    made js, html, and css files

---
+++ app.js
@@ -0,0 +1,3 @@
+const DOMAIN = 'https://openweathermap.org/api';
+const API_KEY = "09f877c9272bc64e109cd676a8422c51";
+const BASE_URL = `api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}`;
